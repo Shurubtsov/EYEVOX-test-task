ee6a85d048eb507d6c129975efc12c8826533cd8
Данила Шурубцов <qlyouqwa4ka@gmail.com>
2022-06-28 14:55:46 +0300

    add models

--- internal/chat/model.go
+++ internal/chat/model.go
@@ -1,1 +1,12 @@
 package chat
+
+type Chat struct {
+	ID              string `json:"id,omitempty"`
+	Name            string `json:"name,omitempty"`
+	FounderNickname string `json:"founder_nickname,omitempty"`
+}
+
+type CreateChatDTO struct {
+	Name            string `json:"name,omitempty"`
+	FounderNickname string `json:"founder_nickname,omitempty"`
+}
--- internal/message/model.go
+++ internal/message/model.go
@@ -1,1 +1,8 @@
 package message
+
+type Message struct {
+	ID              string `json:"id,omitempty"`
+	CreatorNickname string `json:"creator_nickname,omitempty"`
+	ChatID          string `json:"chat_id,omitempty"`
+	TextMessage     string `json:"text_message,omitempty"`
+}
